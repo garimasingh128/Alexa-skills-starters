bafe623f853e356a725d57f49730065a7180f971
dalalvarun <70593335+dalalvarun@users.noreply.github.com>
2021-01-08 23:42:16 +0530

    Unzipped Alexa Skills' folders and added README's for each skill
    Fixes #20 #21 #13

---
+++ Touristy/README.md
@@ -0,0 +1,25 @@
+<h1><b><img src="https://www.clipartmax.com/png/full/193-1934146_%C2%A0-alexa-skills-kit-logo.png" width="25px"> Skill 3 :Touristy✈️</b><h1>
+
+![Author](https://img.shields.io/badge/author-garimasingh128-orange)
+![License](https://img.shields.io/badge/license-MIT-brightgreen)
+![Platform](https://img.shields.io/badge/platform-Visual%20Studio%20Code-blue)
+![Maintained](https://img.shields.io/maintenance/yes/2020)
+[![Join the chat at https://gitter.im/SWOC2021/Alexa-skills-starters](https://badges.gitter.im/SWOC2021/Alexa-skills-starters.svg)](https://gitter.im/SWOC2021/Alexa-skills-starters?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+# __How to invoke the skill?__
+Just say "__Launch touristy__" to invoke it.
+
+# __What can it do?__
+On invocation, it would say :
+"Hello, I can show you some __places to visit in Australia__. Just ask me, places to visit in Australia".  
+
+When you do as directed, it will suggest you places like "__Sydney, Bondi Beach, Gold Coast__". Further it will say "You can __ask me to brief you on any of these cities__". 
+
+On selecting one of the suggestions, Alexa would __tell you more__ about that respective place.
+# __ReadMe contributed by__
+<a href="https://github.com/dalalvarun">![Author](https://img.shields.io/badge/USER-dalalvarun-yellow)</a> ![GitHub followers](https://img.shields.io/github/followers/dalalvarun?style=social)
+
+#
+[![built with love](https://forthebadge.com/images/badges/built-with-love.svg)](https://github.com/garimasingh128) 
+## ❤️ Thanks to the awesome Amazon community!
+
---
+++ Touristy/lambda/index.js
@@ -0,0 +1,182 @@
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require("ask-sdk-core");
+
+const cityDescription = {
+  Sydney:
+    "New South Wales state capital known for its waterfront Sydney Opera House & surfing at Bondi Beach.",
+  Melborne:
+    "Australian state capital with 19th-century architecture, Royal Botanic Gardens & art institutes.",
+  "Gold Coast":
+    "Urban Australian area known for surfing beaches like Snapper Rocks, theme parks & canals.",
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput =
+      "Hello, I can show you some places to visit in Australia. Just ask me, places to visit in Australia.";
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "HelloWorldIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput =
+      "Hello, I can show you some places to visit in Australia. Just ask me, places to visit in Australia.";
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+
+const ListPlacesIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "ListPlacesIntent"
+    );
+  },
+  handle(handlerInput) {
+    const cityArray = Object.keys(cityDescription);
+    let speakOutput = `You can visit places like `;
+    cityArray.map((city) => {
+      speakOutput += city + ", ";
+      return speakOutput;
+    });
+
+    speakOutput += ". You can ask me to brief you on any of these cities.";
+
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+
+const DestinataionDescriptionIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        "DestinataionDescriptionIntent"
+    );
+  },
+  handle(handlerInput) {
+    const askedCity =
+      handlerInput.requestEnvelope.request.intent.slots.cityname.value;
+    let speakOutput = `Sure, ${askedCity} is the ${cityDescription[askedCity]}`;
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "You can say hello to me! How can I help?";
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        "AMAZON.CancelIntent" ||
+        Alexa.getIntentName(handlerInput.requestEnvelope) ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "Goodbye!";
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) ===
+      "SessionEndedRequest"
+    );
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest"
+    );
+  },
+  handle(handlerInput) {
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const speakOutput = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
+        .speak(speakOutput)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~ Error handled: ${error.stack}`);
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloWorldIntentHandler,
+    ListPlacesIntentHandler,
+    DestinataionDescriptionIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
